a69827022caa72a615c1773d984b8cf93b163ef1
Abinaya2409 <44841628+Abinaya2409@users.noreply.github.com>
2019-08-16 19:13:49 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # sudo chmod 755 /var/www/server.js # optional
 # this will restart app/server on instance reboot
-crontab -l | { cat; echo "@reboot pm2 start /var/www/node-test/index.js -i 0 --name \"node-app\""; } | crontab -
-sudo pm2 stop node-app
+crontab -l | { cat; echo "@reboot pm2 start /var/www/nodeservice/index.js -i 0 --name \"node-application\""; } | crontab -
+sudo pm2 stop node-application
 # actually start the server
-sudo pm2 start /var/www/node-test/index.js -i 0 --name "node-app"
+sudo pm2 start /var/www/nodeservice/index.js -i 0 --name "node-application"
