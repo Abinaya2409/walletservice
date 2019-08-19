b4a98be41f7fdf4a43c6e4a7e02b9376874c64de
Abinaya2409 <44841628+Abinaya2409@users.noreply.github.com>
2019-08-19 12:12:31 +0530

    Update variables.js

--- src/config/variables.js
+++ src/config/variables.js
@@ -1,8 +1,8 @@
 "use strict";
 
-const crmAdapterAPI = process.env.CRM_ADAPTER_API;
-const fxFeeGuid = process.env.FX_FEE_GUID;
-const autoNumberId = process.env.AUTO_NUMBER_GUID;
+const crmAdapterAPI = "http://localhost:8081";
+const fxFeeGuid = "e3a19082-70bb-e911-a8b4-00224800449b";
+const autoNumberId = "62730f94-c886-e911-a8b2-002248004c4b";
 
 const variables = {
     crmAdapterAPI,
